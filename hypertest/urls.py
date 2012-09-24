ac479b28fe7bcedff243c3e9234ac9248721b076
Jason Kraus <jasonk@cukerinteractive.com>
2012-09-24 15:05:39 -0700

    s3 integration

--- hypertest/urls.py
+++ hypertest/urls.py
@@ -1,15 +1,21 @@
-from django.conf.urls import patterns, include, url
+import os
 
+from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+from s3resource.resources import S3StorageResource
+
 import hyperadmin
 hyperadmin.autodiscover() #TODO this does nothing
 hyperadmin.site.install_models_from_site(admin.site) #ports admin models to hyperadmin
-hyperadmin.site.install_storage_resources() #enables the storage resource for media and static
+if 'S3_KEY' in os.environ:
+    hyperadmin.site.install_storage_resources(media_resource_class=S3StorageResource) #enables the storage resource for media and static
+else:
+    hyperadmin.site.install_storage_resources() #enables the storage resource for media and static
 
 from dockitcms.sites import site as dockitcmssite
 
